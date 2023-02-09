8a82ad861179df0c2262ac6e4f88c2be6aaa2c21
erjan <dosaev.rysbek5@gmail.com>
2023-02-09 17:03:47 +0600

    requests 02_23 12:09

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
 
-from oneXbet.views import handler404
+from oneXbet.views import handler404,handler500
 
 urlpatterns = [
     path('one/', include('oneXbet.urls')),
@@ -12,5 +12,5 @@ urlpatterns = [
 
 
 handler404 = handler404
-# handler400 = pageNotFound
+handler500 = handler500
 
--- mySite/oneXbet/views.py
+++ mySite/oneXbet/views.py
@@ -8,8 +8,8 @@ def handler404(request, exception):
     return render(request, '404page.html', status=404)
 
 
-def handler500(request, exception):
-    return render(request, '404page.html', status=500)
+def handler500(exception):
+    return render(template_name='404page.html', status=500)
 
 
 def index(request):
