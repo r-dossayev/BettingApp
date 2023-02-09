dc3502914c8fd3254e3346dbc714823fbf6b414c
erjan <dosaev.rysbek5@gmail.com>
2023-02-09 12:09:53 +0600

    requests 02_23 12:09

--- mySite/oneXbet/views.py
+++ mySite/oneXbet/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def handler404(request, exception):
+    return render(request, '404page.html', status=404)
+
+
+def handler500(request, exception):
+    return render(request, '404page.html', status=500)
+
+
+def index(request):
+    return render(request, 'index.html')
